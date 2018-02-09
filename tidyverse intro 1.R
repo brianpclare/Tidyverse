4ba7d9a3967ba5990268f832c8767dd8a2416845
Brian Clare <brianclare.math@gmail.com>
2018-02-09 12:13:38 -0500

    microbenchmark

--- tidyverse intro 1.R
+++ tidyverse intro 1.R
@@ -47,3 +47,24 @@ path <- "hurricane amy.csv"
 tidy <- read_csv(path)
 
 base <- read.csv(path)
+
+# Digression - microbenchmark
+
+library(microbenchmark)
+
+x <- runif(100)
+microbenchmark(sqrt(x),x ^ 0.5)
+
+suppressMessages(
+  microbenchmark(read.csv(path), read_csv(path))
+)
+
+# From Sean Lahman's Baseball Database
+
+baseball <- "C:\\Users\\bpc29\\Documents\\baseballdatabank-2017.1\\baseballdatabank-2017.1\\core\\Teams.csv"
+
+teams <- read_csv(baseball)
+
+suppressMessages(
+  microbenchmark(read.csv(baseball), read_csv(baseball))
+)
