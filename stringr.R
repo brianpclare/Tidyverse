f44997f7a7881d24727fb810301f62ddde50c20f
Brian Clare <brianclare.math@gmail.com>
2018-02-27 14:37:30 -0500

    stringr sheet, regex

---
+++ stringr.R
@@ -0,0 +1,8 @@
+library(stringr)
+
+library(readxl)
+
+#writing to xlsx, especially to different sheets
+library(openxlsx)
+
+# the package "xlsx" is also usable but has a java dependency that can be annoying for installation
