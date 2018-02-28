1c5236245ac21c6d43a933de46c6af0c5c46996a
Brian Clare <brianclare.math@gmail.com>
2018-02-28 00:59:49 -0500

    reduce buoy import, comment stringr and more regex

--- stringr.R
+++ stringr.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 library(readxl)
 
 #writing to xlsx, especially to different sheets
@@ -36,14 +37,14 @@ str_count(str2, " a ")
 str3 <- "Tom said we'll have time to read a tame tome at the bottom of the well"
 
 str_count(str3, "tom")
-str_count(str3, "t.m")
+str_count(str3, "t.m")   # . is a wildcard
 
 # One advantage of stringr instead of base R string operations is 
 # that stringr functions work with pipes
 
 str3 %>% str_to_lower() %>% str_count("t.m")
 
-str3 %>% str_to_lower() %>% str_count("t[oi]m")
+str3 %>% str_to_lower() %>% str_count("t[oi]m")   # [] means any of the characters inside
 
 # [xyz] will match any one of those characters
 # We can use that to match cases as well if we don't want the whole string changed to lower
@@ -54,5 +55,48 @@ str4 <- "Food, fooooood. FOOOOOOOOOOOOD!"
 
 str4 %>% str_to_lower() %>% str_count("fo{2,}d")
 
+
+str4 %>% str_to_lower() %>% str_extract("[[:punct:]]")
 str4 %>% str_to_lower() %>% str_extract_all("[[:punct:]]")
 
+str4 %>% str_count(".")
+
+
+
+
+
+
+
+
+
+str4 %>% str_count("\\.")
+
+# Like how you need to double the [], often in R you need to double escape \\ instead of \
+
+str5 <- "this
+is
+a
+terrible
+poem"
+
+str5 %>% str_count("\n")
+
+
+# Let's look at the storms data again
+storms
+
+# Beginning or end of string
+
+As <- storms %>% filter(str_detect(name, "^A")) %>% select(name, year) %>% unique()
+
+Ms <- storms %>% filter(str_detect(name, "^M")) %>% select(name, year) %>% unique()
+
+end_Ws <- storms %>% filter(str_detect(name, "w$")) %>% select(name, year) %>% unique()
+
+
+
+# Regular Expressions can get pretty messy
+
+rnum <- "XCVIII"
+
+rnum %>% str_detect("^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$")
