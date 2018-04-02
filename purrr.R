fd533b289e2cf74011eb5288ac9ae5e5d61bf3bd
Brian Clare <brianclare.math@gmail.com>
2018-04-02 15:19:00 -0400

    map_df fixed

--- purrr.R
+++ purrr.R
@@ -27,6 +27,10 @@ mtcars %>%
   map_dbl("r.squared")
 
 # repurrrsive gives us some nice list examples, such as got_chars
+# If you're a game of thrones fan, please note this list of characters and traits
+# is based on the books, the TV show would be a little different
+
+# So, you know, spoiler alert
 
 View(got_chars)
 got_chars[1]
@@ -47,9 +51,44 @@ map(got_chars[1:3], 3)
 # Map pipes well because the first argument is the data, apply functions don't all work like that
 
 got_chars[1:3] %>% map(3)
-got_chars %>% map(alive == TRUE)
+alive <- got_chars[got_chars %>% map("alive") == TRUE] %>% map("name")
+dead <- got_chars[got_chars %>% map("alive") == FALSE] %>% map("name")
+
+alive <- tibble(alive)
+colnames(alive) <- "Name"
+alive$status <- "Alive"
+
+dead <- tibble(dead)
+colnames(dead) <- "Name"
+dead$status <- "Dead"
+
+status <- bind_rows(alive, dead)
+
+status %>% ggplot(aes(x = status)) + geom_bar()
+
+# Did you know you could pipe %>% into ggplot? Sometimes useful
+# It's a little annoying to go %>% into +
+# ggvis is a package that is in some ways an updated ggplot that uses %>%  and never +
+# But ggplot is still pretty much the standard graph package for R
+
+
+# That was a lot of work from those lists to make a simple bar plot though, right?
 
 
 # map_df can take a list of lists and return a dataframe (a tibble)
 
-map_df(got_chars, extract, c("name", "culture", "gender", "id", "born", "alive"))
+status2 <- map_df(got_chars, magrittr::extract, c("name", "alive"))
+
+# Let's turn the true and false into alive and dead though
+
+status2$alive <- ifelse(status2$alive, "Alive", "Dead")
+
+
+# extract is a function in tidyr and in magrittr, if you don't specify then R tries the function
+# from tidyr and gives you an error, so we specify magrittr and it works fine
+
+# Alternate version from base R
+# This can be hard to type in correctly because of the ``s
+
+status3 <- map_df(got_chars, `[`, c("name", "alive"))
+
