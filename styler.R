e59a47b50201974cae63da74272d4bbeef7331b2
Brian Clare <brianclare.math@gmail.com>
2018-09-17 14:57:22 -0400

    styler init

---
+++ styler.R
@@ -0,0 +1,9 @@
+##let's look at some poorly styled code
+
+x<-15;y<-2
+for(i in 1:20){if(i==2){x<-10;y<-x^2}else{y<-8}}
+
+
+# style guides
+# https://google.github.io/styleguide/Rguide.xml
+# http://style.tidyverse.org/
